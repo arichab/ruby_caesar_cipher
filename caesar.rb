c8681165ec359ad6362798fc7c40f8be9804bce5
Ariel Chab <arielchab@gmail.com>
2020-12-01 17:01:36 -0300

    Upload

---
+++ caesar.rb
@@ -0,0 +1,20 @@
+
+def translate(message, shift)
+  result = ""
+  message.each_char do |char|
+     base = char.ord < 91 ? 65:97
+     if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+      rotation = (((char.ord - base) +shift)%26) + base  
+      result += rotation.chr
+     else
+      result += char
+     end
+    end
+    p result
+  end
+  p "What do you want to cipher?"
+  text = gets.chomp
+  p "in wich shift? (1 to 26)"
+  shif = gets.chomp.to_i
+  translate(text, shif)
+  
